2962303762ff5b73f4665ea3301e053dd0896b63
Orest Hlavatskyi <_>
2025-01-18 15:52:00 +0200

    Added remove_button/read_selection to book-card. Fixed some bugs

--- srcipt.js
+++ srcipt.js
@@ -9,6 +9,7 @@ let pagesInput = document.getElementById('pages');
 let haveReadInput = document.getElementById('haveRead');
 
 bookForm.style.display = 'none';
+let bookFormFlag = false;
 
 const myLibrary = [];
 
@@ -18,41 +19,74 @@ function Book(title, author, pages, haveRead){
     this.pages = pages;
     this.haveRead = haveRead;
     this.returnBookDescription = function(){
-        return `${author}, ${pages} pages, ${haveRead}`;
+        return `${author}, ${pages} pages,`;
     }
 }
 
 function addBookToLibrary(){
     let newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, haveReadInput.value);
+    newBook.id = `book${myLibrary.length+1}`;
     if(newBook.title && newBook.author && newBook.pages && newBook.haveRead){
         myLibrary.push(newBook);
         displayBook(newBook);
     }
 }
 
-function openBookForm(){
-    bookForm.style.display = 'block';
+function removeBookFromLibrary(id){
+    let rem = myLibrary.findIndex(book => book.id === id);
+    if(rem !== -1) {
+        myLibrary.splice(rem, 1);
+    }
 }
 
-
-function closeBookForm(){
-    bookForm.style.display = 'none';
+function openCloseBookForm(){
+    bookForm.style.display = !bookFormFlag ? 'block' : 'none';
+    bookFormFlag = !bookFormFlag;
 }
-
 
 function displayBook(book){
     let bookElement = document.createElement('div')
     bookElement.className = "book-card";
+    bookElement.id = book.id;
     let bookTitle = document.createElement('h2');
     let bookDescription = document.createElement('p');
+    let bookRemoveButton = document.createElement('button');
+    bookRemoveButton.className = "book-remove-button";
+    let bookReadSelect = document.createElement('select');
+    let haveReadOption = document.createElement('option');
+    let haveNotReadOption = document.createElement('option');
+
+    haveReadOption.value = 'read';
+    haveReadOption.text = 'Have Read';
+    haveNotReadOption.value = 'not read';
+    haveNotReadOption.text = 'Not Read';
+
+    if(book.haveRead == 'read'){
+        haveReadOption.selected = true;
+    }
+    else {
+        haveNotReadOption.selected = true;
+    }
+
+    bookRemoveButton.addEventListener('click', () => {
+        let parent = bookRemoveButton.parentElement;
+        removeBookFromLibrary(parent.id);
+        parent.remove();
+    })
 
     bookDescription.innerHTML = book.returnBookDescription();
     bookTitle.innerHTML = book.title;
+    bookRemoveButton.innerHTML = "X";
 
     bookElement.appendChild(bookTitle);
     bookElement.appendChild(bookDescription);
+    bookReadSelect.appendChild(haveNotReadOption);
+    bookReadSelect.appendChild(haveReadOption);
+    bookElement.appendChild(bookReadSelect);
+    bookElement.appendChild(bookRemoveButton);
+
     bookContainer.appendChild(bookElement);
 }
 
 addBookButton.addEventListener("click", () => {addBookToLibrary()});
-openFormButton.addEventListener("click", function(){openBookForm()});
+openFormButton.addEventListener("click", function(){openCloseBookForm()});
